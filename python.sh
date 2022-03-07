60267d0b2c48bf595f7d8c423ca909913fedce6f
redwan-hossain <redwan.eng.cou@outlook.com>
2022-03-07 19:29:50 +0600

    removed dangerous codes that mess with gui

--- python.sh
+++ python.sh
@@ -5,7 +5,4 @@ echo Installing python 3.10; sleep 2
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
 sudo apt-get install python3.10 -y
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
-sudo update-alternatives --config python3
-python3 -V
+python3.10 -V
