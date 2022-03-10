e91c281fe782332ea9dd72d8ad658988c158afea
redwan-hossain <redwan.eng.cou@outlook.com>
2022-03-10 23:03:39 +0600

    Added spice to fix hiccups

--- python.sh
+++ python.sh
@@ -3,6 +3,7 @@
 if [[ "$(grep -iR deadsnakes/ppa /etc/apt)" = "" ]]; then
   echo "Installing python 3.10"
   sleep 1
+  sudo apt install software-properties-common -y
   sudo add-apt-repository ppa:deadsnakes/ppa -y
   sudo apt update
   sudo apt-get install python3.10 -y
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-• Simply run `sudo sh python.sh` in the terminal.
+• Simply run `bash python.sh` in the terminal.
 
 • Make sure you running the terminal from the same directory where python.sh file is located.
