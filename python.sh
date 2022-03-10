431915a6745b5ab447d76e4341b8bda096a54301
redwan-hossain <redwan.eng.cou@outlook.com>
2022-03-10 22:55:27 +0600

    Revamped the script with conditional statements

--- python.sh
+++ python.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/bash
+#! /usr/bin/bash
 
-echo Installing python 3.10; sleep 2
+if [[ "$(grep -iR deadsnakes/ppa /etc/apt)" = "" ]]; then
+  echo "Installing python 3.10"
+  sleep 1
+  sudo add-apt-repository ppa:deadsnakes/ppa -y
+  sudo apt update
+  sudo apt-get install python3.10 -y
+  python3.10 -V
+else
+  echo "Already Installed"
+fi
 
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt update
-sudo apt-get install python3.10 -y
-python3.10 -V
